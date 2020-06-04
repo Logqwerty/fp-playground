4b0a3e7a711ce388e104aaa57c56b5ef728e3917
Logqwerty <inm2790@gmail.com>
2020-06-04 20:40:19 +0900

    [Config] 초기 웹팩 설정
    css 파일을 모듈로 사용하기 위해 css-loader, style-loader 추가.
    
    웹팩을 실행할 때마다 dist 폴더를 정리하기 위해 clean-webpack-
    plugin 추가.
    
    빌드된 결과를 자동으로 HTML에 추가하기 위한 html-webpack-plugin
    추가.

---
+++ packages/form-validator/webpack.config.js
@@ -0,0 +1,27 @@
+const path = require("path");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "[name].[chunkhash].js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: "./template/index.html",
+    }),
+  ],
+  mode: "production",
+  optimization: { minimizer: [] },
+};
