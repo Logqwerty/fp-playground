2ea6c2f47feaacb6231d9381c9f50d81785781a9
Logqwerty <inm2790@gmail.com>
2020-06-11 19:17:24 +0900

    [Feat] 필수 입력 값에 대한 validation 기능 추가
    필수 입력 필드에 값을 입력했는지 판단하기 위한 validation 로직 추가.
    
    만약 값이 존재한다면, 입력 필드의 경계선이 초록색으로 나타남. 그렇지 않으면,
    경계선은 붉은색으로 나타나고, 에러 메시지를 보여줌.

--- packages/form-validator/src/form/index.js
+++ packages/form-validator/src/form/index.js
@@ -6,14 +6,84 @@ import $ from "../utils/DOM";
 
 const Form = {};
 
-Form.render = () =>
-  R.pipe(
-    $.makeEl,
-    $.append($.qs("#root")),
-    $.on('submit', e => {
-      e.preventDefault();
-      console.log(e.currentTarget);
-    }),
-  )(formTemplate);
+Form.render = () => R.pipe(
+  $.makeEl,
+  $.append($.qs("#root")),
+  $.on("submit", e => {
+    e.preventDefault();
+
+    R.pipe(
+      R.tap(checkUsername),
+      R.tap(checkEmail),
+      R.tap(checkPassword),
+      R.tap(checkPasswordConfirm),
+    )(e.currentTarget);
+  }),
+)(formTemplate);
 
 export default Form;
+
+const getFieldName = $el => $el.id.charAt(0).toUpperCase() + $el.id.slice(1);
+
+const showError = R.forEach($el =>
+  R.pipe(
+    R.prop("parentElement"),
+    R.tap($.removeClass('success')),
+    R.tap($.addClass('error')),
+    R.tap(R.pipe(
+      $.find(".error-message"),
+      $.text(`${getFieldName($el)} is required`)
+    ))
+  )($el)
+)
+
+const showSuccess = R.forEach(
+  R.pipe(
+    R.prop("parentElement"),
+    R.tap($.removeClass('error')),
+    R.tap($.addClass('success')),
+  )
+)
+
+const truthyCase = R.pipe(
+  R.take(1),
+  R.flatten,
+)
+
+const falsyCase = R.pipe(
+  R.takeLast(1),
+  R.flatten,
+)
+
+const checkIsEmpty = R.pipe(
+  $el => [$el],
+  R.partition($el => $el.value.trim().length > 0),
+  R.tap(R.pipe(
+    truthyCase,
+    showSuccess,
+  )),
+  R.tap(R.pipe(
+    falsyCase,
+    showError,
+  )),
+);
+
+const checkUsername = R.pipe(
+  $.find("#username"),
+  checkIsEmpty,
+)
+
+const checkEmail = R.pipe(
+  $.find("#email"),
+  checkIsEmpty,
+)
+
+const checkPassword = R.pipe(
+  $.find("#password"),
+  checkIsEmpty,
+)
+
+const checkPasswordConfirm = R.pipe(
+  $.find("#password-confirm"),
+  checkIsEmpty,
+)
